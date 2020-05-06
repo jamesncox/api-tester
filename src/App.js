a13be1a2e2903a91cb49c4343864f8f79a8b06a5
jamesncox <jamesncox@yahoo.com>
2020-05-06 09:37:57 -0500

    Update App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Container from "./Container"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>API Tester</h1>
+      <h2>Enter an API URL below to test an endpoint</h2>
+      <Container />
     </div>
   );
 }
-
-export default App;
